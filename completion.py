f4693c115cede3b45e7506b38ddf6d92f8c4e0dd
delima87 <delima_87@hotmail.com>
2021-10-15 14:04:06 +0200

    completion_v2

--- completion.py
+++ completion.py
@@ -21,7 +21,7 @@ def save_visuals(visuals,aspect_ratio,image_path, image_dir):
         util.save_image(im, save_path, aspect_ratio=aspect_ratio)
     return images
 
-def estimate_completion(name_model,checkpoints):
+def estimate_completion(name_model,checkpoints,dataroot):
     opt = TestOptions().parse()  # get test options
     # hard-code some parameters for test
     opt.num_threads = 0   # test code only supports num_threads = 0
@@ -29,7 +29,7 @@ def estimate_completion(name_model,checkpoints):
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
     opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
-    opt.dataroot = "./temp"
+    opt.dataroot = dataroot
     opt.name = name_model
     opt.checkpoints_dir = checkpoints
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
