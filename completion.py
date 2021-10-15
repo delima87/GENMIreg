eec1f718f2d49dc3b1915e9c2b89f24e3f2c496d
delima87 <delima_87@hotmail.com>
2021-10-15 13:36:18 +0200

    test_init

---
+++ completion.py
@@ -0,0 +1,67 @@
+
+import os
+from PIL.Image import LANCZOS
+from options.test_options import TestOptions
+from data import create_dataset
+from models import create_model
+from util.visualizer import save_images
+from util import util
+import ntpath
+
+def save_visuals(visuals,aspect_ratio,image_path, image_dir):
+    short_path = ntpath.basename(image_path[0])
+    name = os.path.splitext(short_path)[0]
+    images = []
+    for label, im_data in visuals.items():
+        im = util.tensor2im(im_data)
+        if label == "real_A" or label == "fake_B":
+            images.append(im)
+        image_name = '%s_%s.png' % (name, label)
+        save_path = os.path.join(image_dir, image_name)
+        util.save_image(im, save_path, aspect_ratio=aspect_ratio)
+    return images
+
+def estimate_completion(name_model):
+    opt = TestOptions().parse()  # get test options
+    # hard-code some parameters for test
+    opt.num_threads = 0   # test code only supports num_threads = 0
+    opt.batch_size = 1    # test code only supports batch_size = 1
+    opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
+    opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
+    opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
+    opt.dataroot = "./temp"
+    opt.name = name_model
+    dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
+    model = create_model(opt)      # create a model given opt.model and other options
+    model.setup(opt)               # regular setup: load and print networks; create schedulers
+    # create a website
+    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
+    if opt.load_iter > 0:  # load_iter is 0 by default
+        web_dir = '{:s}_iter{:d}'.format(web_dir, opt.load_iter)
+    print('creating web directory', web_dir)
+    # test with eval mode. This only affects layers like batchnorm and dropout.
+    # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
+    # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
+    final_images = []
+    if opt.eval:
+        model.eval()
+    for i, data in enumerate(dataset):
+        if i >= opt.num_test:  # only apply our model to opt.num_test images.
+            break
+        model.set_input(data)  # unpack data from data loader
+        model.test()           # run inference
+        visuals = model.get_current_visuals()  # get image results
+        img_path = model.get_image_paths()     # get image paths
+        if i % 5 == 0:  # save images to an HTML file
+            print('processing (%04d)-th image... %s' % (i, img_path))
+        # save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
+        c_img = save_visuals(visuals,opt.aspect_ratio,img_path,web_dir)
+        final_images.extend(c_img)
+    return final_images
+
+
+if __name__ == '__main__':
+    estimate_completion("feather_remote_sensing")
+    print("DONE")
+    
+    
