d4c0e97cb653cc0b41f367350ddda88f00c348c6
delima87 <delima_87@hotmail.com>
2021-10-15 20:52:51 +0200

    ready

--- completion.py
+++ completion.py
@@ -5,42 +5,68 @@ from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
 from util import util
+import torch
 import ntpath
 
-def save_visuals(visuals,aspect_ratio,image_path, image_dir):
-    short_path = ntpath.basename(image_path[0])
-    name = os.path.splitext(short_path)[0]
+def save_visuals(visuals):
     images = []
     for label, im_data in visuals.items():
         im = util.tensor2im(im_data)
         if label == "real_A" or label == "fake_B":
             images.append(im)
-        image_name = '%s_%s.png' % (name, label)
-        save_path = os.path.join(image_dir, image_name)
-        util.save_image(im, save_path, aspect_ratio=aspect_ratio)
     return images
 
-def estimate_completion(name_model,checkpoints,dataroot):
-    opt = TestOptions().parse()  # get test options
-    # hard-code some parameters for test
+def predict_completion(dataroot,modelName, checkpointns_dir):
+    opt = TestOptions()  # get test options
     opt.num_threads = 0   # test code only supports num_threads = 0
     opt.batch_size = 1    # test code only supports batch_size = 1
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
     opt.no_flip = True    # no flip; comment this line if results on flipped images are needed.
     opt.display_id = -1   # no visdom display; the test code saves the results to a HTML file.
     opt.dataroot = dataroot
-    opt.name = name_model
-    opt.checkpoints_dir = checkpoints
+    opt.name = modelName
+    opt.checkpoints_dir = checkpointns_dir
+    opt.dataset_mode = "unaligned"
+    opt.phase = "test"
+    opt.max_dataset_size = float("inf")
+    opt.direction = 'AtoB'
+    opt.input_nc = 3
+    opt.output_nc = 3
+    opt.preprocess = "resize_and_crop"
+    opt.load_size = 256
+    opt.crop_size = 256
+    opt.model = "cycle_gan"
+    opt.no_flip = True
+    opt.isTrain = False
+    opt.display_winsize = 256
+    opt.epoch = "latest"
+    opt.verbose = True
+    opt.suffix = ""
+    opt.ngf = 64
+    opt.ndf = 64
+    opt.netG = "resnet_9blocks"
+    opt.netD = "basic"
+    opt.norm = "instance"
+    opt.no_dropout = True
+    opt.init_type = "normal"
+    opt.init_gain = 0.02
+    opt.gpu_ids = '0'
+    opt.load_iter = 0
+    str_ids = opt.gpu_ids.split(',')
+    opt.gpu_ids = []
+    opt.eval = True
+    opt.num_test = 50
+    opt.aspect_ratio = 1.0
+    opt.n_layers_D = 3
+    for str_id in str_ids:
+        id = int(str_id)
+        if id >= 0:
+            opt.gpu_ids.append(id)
+    if len(opt.gpu_ids) > 0:
+        torch.cuda.set_device(opt.gpu_ids[0])
     dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
-    model = create_model(opt)      # create a model given opt.model and other options
-    model.setup(opt)               # regular setup: load and print networks; create schedulers
-    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
-    if opt.load_iter > 0:  # load_iter is 0 by default
-        web_dir = '{:s}_iter{:d}'.format(web_dir, opt.load_iter)
-    print('creating web directory', web_dir)
-    # test with eval mode. This only affects layers like batchnorm and dropout.
-    # For [pix2pix]: we use batchnorm and dropout in the original pix2pix. You can experiment it with and without eval() mode.
-    # For [CycleGAN]: It should not affect CycleGAN as CycleGAN uses instancenorm without dropout.
+    model = create_model(opt)      # create a model gi
+    model.setup(opt)  
     final_images = []
     if opt.eval:
         model.eval()
@@ -54,9 +80,13 @@ def estimate_completion(name_model,checkpoints,dataroot):
         if i % 5 == 0:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
         # save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
-        c_img = save_visuals(visuals,opt.aspect_ratio,img_path,web_dir)
+        # web_dir = os.path.join("results", opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
+        c_img = save_visuals(visuals)
         final_images.extend(c_img)
-    return final_images
-
-    
+    print("Process correctly done", len(final_images))
+if __name__ == "__main__":
+    checkpoints = "./checkpoints"
+    name_model = "feather_remote_sensing" 
+    data = "./temp"
+    predict_completion(data,name_model,checkpoints)
     
